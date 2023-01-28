5d50c97b0c96aa2b1481a44882712668c5dac1a3
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-01-28 16:34:15 +0200

    GenericsLab

---
+++ SoftUni-C-Sharp-Advanced/Generics - Lab/Box/Box/BoxOfT.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        private List<T> data;
+        private int count;
+        public BoxOfT()
+        {
+            this.Data = new List<T>();
+            count = this.Data.Count;
+        }
+        public List<T> Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public void Add(T item)
+        {
+            Data.Add(item);
+        }
+
+        public int Count()
+        {
+            var count = this.Data.Count;
+            return count;
+        }
+
+        public T Remove()
+        {
+            var lastItem = this.Data[this.Data.Count - 1];
+            data.RemoveAt(this.Data.Count - 1);
+            return lastItem;
+        }
+    }
+}
---
+++ SoftUni-C-Sharp-Advanced/Generics - Lab/GenericScale/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = new EqualityScale<int>(5, 6);
+
+            Console.WriteLine(n.AreEqual());
+        }
+    }
+}
