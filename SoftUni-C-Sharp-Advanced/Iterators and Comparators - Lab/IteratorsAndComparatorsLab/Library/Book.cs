9ab326e6784320a52e5caf359641d2a42ba6e7f9
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-01 16:37:46 +0200

    IteratorsAndComparatorsLab

---
+++ SoftUni-C-Sharp-Advanced/Iterators and Comparators - Lab/IteratorsAndComparatorsLab/Library/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        private string title;
+        private int year;
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+    }
+}
---
+++ SoftUni-C-Sharp-Advanced/Iterators and Comparators - Lab/IteratorsAndComparatorsLab/Library/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator
+    {
+        private readonly List<Book> books;
+        public IEnumerable<Book> GetEnumerator()
+        {
+            for (int i = 0; i < this.books.Count; i++)
+                yield return this.books[i];
+        }
+    }
+
+}
