6d60fd6af5bf33bedcb4513b80e336a4e0b37d65
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-23 18:33:52 +0200

    Restaurant
    Project with the following classes and hierarchy.

---
+++ Softuni-C-Sharp-OPP/Inheritance - Exercise/Restaurant/Beverage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        private double milliters;
+        public Beverage(string name, decimal price, double milliters) : base(name, price)
+        {
+            Milliters = milliters;
+        }
+
+        public double Milliters
+        {
+            get { return milliters; }
+            set { milliters = value; }
+        }
+    }
+}
---
+++ Softuni-C-Sharp-OPP/Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double Coffee_Milliters = 50;
+        private const decimal Coffee_Price = 3.50m;
+        private double caffeine;
+        public Coffee(string name, double caffeine) : base(name, Coffee_Price, Coffee_Milliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine
+        {
+            get { return caffeine; }
+            set { caffeine = value; }
+        }
+    }
+}
