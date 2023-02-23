ddb09ee090b42243901c8580e99bd9cb39afc372
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-23 18:30:26 +0200

    PlayersAndMonsters
    Has to create following game hierarchy.

---
+++ Softuni-C-Sharp-OPP/Inheritance - Exercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            int level = int.Parse(Console.ReadLine());
+
+            SoulMaster soulmaster = new SoulMaster(username, level);
+
+            Console.WriteLine(soulmaster.ToString());
+        }
+    }
+}
