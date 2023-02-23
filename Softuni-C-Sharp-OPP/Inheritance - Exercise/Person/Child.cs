88d1e7fde604d13b603e7ad464fd6a0e03098506
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-23 18:24:46 +0200

    InheritanceExercises
    First exercise application for storing data about people.

---
+++ Softuni-C-Sharp-OPP/Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public override int Age
+        {
+            get { return base.Age; }
+            set
+            {
+                if(value <= 15 && value > 0)
+                    base.Age = value;
+            }
+        }
+    }
+}
---
+++ Softuni-C-Sharp-OPP/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {Name}, Age: {Age}"));
+            return stringBuilder.ToString();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public virtual int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value > 0)
+                    this.age = value;
+            }
+        }
+
+    }
+}
