9be4a0a622065e119f4b0dea335bf5f34cbff571
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-24 20:04:10 +0200

    FirstAndReserveTeam
    Created Team class. The Team class has a first team and a reserve team. In the StartUp class, it checks where to add a player.

---
+++ Softuni-C-Sharp-OPP/Encapsulation - Lab/Team/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public decimal IncreaseSalary(decimal percentage)
+        {
+            if(Age < 30)
+            {
+                return Salary += (Salary * (percentage * 0.01m)) / 2;
+            }
+            else
+            {
+                return Salary += Salary * (percentage * 0.01m);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
