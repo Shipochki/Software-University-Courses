6b11417be37766d755a4e0c9c7653d4ba91f8e20
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-28 20:33:43 +0200

    Cars
    Hierarchy with Interface for Car and Electric Car

---
+++ Softuni-C-Sharp-OPP/Interfaces and Abstraction - Lab/Cars/ICar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return string.Empty;
+        }
+
+        public string Stop()
+        {
+            return string.Empty;
+        }
+    }
+}
