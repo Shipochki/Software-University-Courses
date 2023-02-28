aeeab2a6f7c82fc1ff880d42a4312b7af5abb7a4
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-02-28 20:44:25 +0200

    BorderControl
    Can receive an unknown amount of lines from the console until the command "End" is received, on each line, there will be a piece of information for either a citizen or a robot who tries to enter your city. After the "End" command on the next line, you have to write a single number representing the last digits of fake ids. The output of program consist all of detained Ids each on a separate line in the order of input.

---
+++ Softuni-C-Sharp-OPP/Interfaces and Abstraction - Exercise/BorderControl/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Person : ICitizen
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age, string iD)
+        {
+            this.name = name;
+            this.age = age;
+            ID = iD;
+        }
+
+        public string ID { get; set; }
+    }
+}
---
+++ Softuni-C-Sharp-OPP/Interfaces and Abstraction - Exercise/BorderControl/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<ICitizen> citizens = new List<ICitizen>();
+
+            string inputData = Console.ReadLine();
+
+            while (inputData != "End")
+            {
+                var splitedData = inputData.Split().ToList();
+
+                if(splitedData.Count == 3)
+                {
+                    string name = splitedData[0];
+                    int age = int.Parse(splitedData[1]);
+                    string id = splitedData[2];
+                    Person person = new Person(name, age, id);
+                    citizens.Add(person);
+                }
+                else
+                {
+                    string model = splitedData[0];
+                    string id = splitedData[1];
+                    Robot robot = new Robot(model, id);
+                    citizens.Add(robot);
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            string lastDigit = Console.ReadLine();
+
+            foreach (var citizen in citizens)
+            {
+                if(citizen.ID.EndsWith(lastDigit))
+                    Console.WriteLine(citizen.ID);
+            }
+        }
+    }
+}
