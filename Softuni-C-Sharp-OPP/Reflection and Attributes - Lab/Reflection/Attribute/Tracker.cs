84fbc185c7cab47e4a2c19a90a62e2146eda99c6
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-03-02 18:08:55 +0200

    AuthorsAtribute

---
+++ Softuni-C-Sharp-OPP/Reflection and Attributes - Lab/Reflection/Attribute/Tracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (MethodInfo method in methods)
+            {
+                if(method.CustomAttributes.Any(m => m.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine("{0} is written by {1}",
+                        method.Name, attr.Name);
+                    }
+                }
+            }
+        }
+    }
+}
