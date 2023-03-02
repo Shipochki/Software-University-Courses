ee46509aee13706c43bc8ee1e0695895ff75c7f7
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-03-02 18:15:25 +0200

    CommandPattern
    Command interface - ICommand, witch contains a method Execute.
    CommandObjects - HelloCommand, ExitCommand.
    Command interpreter interface ICommandInterpreter with method Read.
    Command Interpreter Class with purpose to read commands, execute them and return result.
    Class Engine with method void Run
    with private readonly IcommandInterpreter.

---
+++ Softuni-C-Sharp-OPP/Reflection and Attributes - Exercise/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var splitedArgs = args.Split().ToArray();
+            string commandName = splitedArgs[0];
+            string[] arrArgs = splitedArgs.Skip(1).ToArray();
+            
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            if (commandType == null)
+                throw new InvalidOperationException("Invalid command type!");
+
+            MethodInfo excecuteMethodInfo = commandType
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .FirstOrDefault(m => m.Name == "Execute");
+
+            if (excecuteMethodInfo == null)
+                throw new InvalidOperationException("Command does not implement required pattern! Try implementing IComand interface instead!");
+
+            object cmdInstance = Activator.CreateInstance(commandType);
+            return (string)excecuteMethodInfo.Invoke(cmdInstance, new object[] { arrArgs });
+        }
+    }
+}
---
+++ Softuni-C-Sharp-OPP/Reflection and Attributes - Exercise/CommandPattern/Engine.cs
@@ -0,0 +1,26 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter intrepeter;
+        public Engine(ICommandInterpreter intrepeter)
+        {
+            this.intrepeter = intrepeter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+                Console.WriteLine(this.intrepeter.Read(inputLine));
+            }
+        }
+    }
+}
