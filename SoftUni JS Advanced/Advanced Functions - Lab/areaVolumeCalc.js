f8e9e5713007816348e1ad7f8af9753dc42efee4
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-03-21 16:57:32 +0200

    Advanced Functions - Lab
    Exercise

---
+++ SoftUni JS Advanced/Advanced Functions - Lab/areaVolumeCalc.js
@@ -0,0 +1,28 @@
+function area() {
+    return Math.abs(this.x * this.y);
+}
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+}
+
+function solve(area, vol, input) {
+    let objects = JSON.parse(input);
+    let arr = [];
+
+    objects.forEach(obj => {
+        arr.push({
+            area: area.call(obj),
+            volume: vol.call(obj),
+        })
+    });
+
+    return arr;
+}
+
+console.log(solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`
+    ));
