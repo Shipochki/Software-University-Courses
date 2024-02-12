cdcb5a873ccdf1f0dff4166d2b9471be17d92f05
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-02-12 12:59:46 +0200

    Setup the database entities

---
+++ SoftUni C-Sharp Web Development Basics/Exam - 17 June 2023/Homies/Data/HomiesDbContext.cs
@@ -0,0 +1,63 @@
+﻿using Homies.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Data
+{
+    public class HomiesDbContext : IdentityDbContext
+    {
+        public HomiesDbContext(DbContextOptions<HomiesDbContext> options)
+            : base(options)
+        {
+        }      
+        
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Models.Type> Types { get; set; }
+
+        public DbSet<EventParticipant> EventsParticipants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Models.Type>()
+                .HasData(new Models.Type()
+                {
+                    Id = 1,
+                    Name = "Animals"
+                },
+                new Models.Type()
+                {
+                    Id = 2,
+                    Name = "Fun"
+                },
+                new Models.Type()
+                {
+                    Id = 3,
+                    Name = "Discussion"
+                },
+                new Models.Type()
+                {
+                    Id = 4,
+                    Name = "Work"
+                });
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasKey(e => new { e.HelperId, e.EventId });
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(e => e.Helper)
+                .WithMany()
+                .HasForeignKey(e => e.HelperId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(e => e.Event)
+                .WithMany()
+                .HasForeignKey(e => e.EventId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+	}
+}
