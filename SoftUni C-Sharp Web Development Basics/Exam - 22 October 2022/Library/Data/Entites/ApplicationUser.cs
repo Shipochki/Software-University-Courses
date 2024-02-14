3d903aa749d16e3dc925d57504aabb36bf19327c
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-02-14 17:19:36 +0200

    Created database and entities

---
+++ SoftUni C-Sharp Web Development Basics/Exam - 22 October 2022/Library/Data/Entites/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Entites
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		[MaxLength(20)]
+		public override string UserName 
+		{ 
+			get => base.UserName; 
+			set => base.UserName = value; 
+		}
+
+		[Required]
+		[MaxLength(60)]
+		public override string Email 
+		{ 
+			get => base.Email;
+			set => base.Email = value; 
+		}
+
+		public List<ApplicationUserBook> ApplicationUsersBooks { get; set; } = new List<ApplicationUserBook>();
+	}
+}
